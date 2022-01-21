79f0c001a1cf5d130016a625e5abd2b74c7cc6b5
wonjeonghwa <90661415+wonjeonghwa@users.noreply.github.com>
2022-01-21 16:02:00 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,12 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+// important! for alignment, you should make things
+// relative to the canvas' current width/height.
+
+function draw() {
+  var ctx = (a canvas context);
+  ctx.canvas.width  = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+  //...drawing code...
+}
